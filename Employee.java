4a58f5ef19712dff83ad99ec6675f82bccebc8ff
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-29 09:38:56 -0500

    save

--- Employee.java
+++ Employee.java
@@ -7,12 +7,14 @@ public class Employee {
     String name;
     int id;
     double salary;
+    String Location;
  
    //constructor 
-public Employee(String name, int id, double salary) {
+public Employee(String name, int id, double salary, String Location) {
          this.name = name;
          this.id = id;
          this.salary = salary;
+         this.Location=Location;
      }
   
     //Method to display employee details
@@ -21,12 +23,13 @@ public Employee(String name, int id, double salary) {
             System.out.println("Employee ID :" +id);
             System.out.println("Employee name" +name);
             System.out.println("Salary:₹" +salary);
+            System.out.println("Location:₹" +Location);
         }
 
    //Main method 
    public static void main(String[] args) {
     //Creating an object of employee
-    Employee emp1 = new Employee("Kos",101,50000.0);
+    Employee emp1 = new Employee("Kos",101,50000.0,"Hubli");
     emp1.displayInfo();
    }
 
